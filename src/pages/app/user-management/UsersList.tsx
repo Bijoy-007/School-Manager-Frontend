278dbeb17a166962bd34d0c3f62a996c60ae89fd
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2022-04-19 11:35:12 +0530

    table ui enhanced

--- src/pages/app/user-management/UsersList.tsx
+++ src/pages/app/user-management/UsersList.tsx
@@ -1,6 +1,6 @@
-import { Table } from 'antd';
+import { Button, Table } from 'antd';
 import { ColumnType } from 'antd/lib/table';
-import { tableColumnTextFilterConfig } from '../../../components/app/UserList/TableUtils';
+// import { tableColumnTextFilterConfig } from '../../../components/app/UserList/TableUtils';
 
 interface UserData {
   key: string;
@@ -8,9 +8,172 @@ interface UserData {
   name: string;
   joiningDate: string;
   designation: string;
+  engagementType: string;
+  status: string;
 }
 
-// interface CustomColumn {
+const columns: ColumnType<UserData>[] = [
+  {
+    title: 'Teacher Id',
+    dataIndex: 'teacherId',
+    sorter: (a, b) => {
+      return a.teacherId > b.teacherId ? 1 : -1;
+    },
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Designation',
+    dataIndex: 'designation',
+    filters: [
+      {
+        text: 'Principal',
+        value: 'Principal',
+      },
+      {
+        text: 'Senior Teacher',
+        value: 'Senior Teacher',
+      },
+      {
+        text: 'Assistant Teacher',
+        value: 'Assistant Teacher',
+      },
+    ],
+    filterMode: 'tree',
+    filterSearch: true,
+    onFilter: (value, record) =>
+      record.designation.indexOf(value.toString()) === 0,
+  },
+  {
+    title: 'Joining Date',
+    dataIndex: 'joiningDate',
+    sorter: (a, b) => {
+      return new Date(a.joiningDate) > new Date(b.joiningDate) ? 1 : -1;
+    },
+  },
+  {
+    title: 'Engagement Type',
+    dataIndex: 'engagementType',
+    filters: [
+      {
+        text: 'Permanent',
+        value: 'Permanent',
+      },
+      {
+        text: 'Temporary',
+        value: 'Temporary',
+      },
+    ],
+    onFilter: (value, record) =>
+      record.engagementType.indexOf(value.toString()) === 0,
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    dataIndex: 'status',
+    render: (text, record) =>
+      record.status === 'Active' ? (
+        <Button
+          onClick={activatehandler.bind(this, record)}
+          type="primary"
+          shape="round"
+          danger
+        >
+          Diactivate
+        </Button>
+      ) : (
+        <Button
+          onClick={activatehandler.bind(this, record)}
+          type="primary"
+          shape="round"
+        >
+          Activate
+        </Button>
+      ),
+  },
+];
+
+const data: UserData[] = [
+  {
+    key: '1',
+    teacherId: 'T005',
+    name: 'John Brown',
+    joiningDate: '01/01/2019',
+    designation: 'Principal',
+    engagementType: 'Permanent',
+    status: 'Active',
+  },
+  {
+    key: '2',
+    teacherId: 'T002',
+    name: 'Brown',
+    joiningDate: '01/10/2020',
+    designation: 'Senior Teacher',
+    engagementType: 'Permanent',
+    status: 'Inactive',
+  },
+  {
+    key: '3',
+    teacherId: 'T003',
+    name: 'Jon',
+    joiningDate: '11/01/2020',
+    designation: 'Assistant Teacher',
+    engagementType: 'Temporary',
+    status: 'Active',
+  },
+  {
+    key: '4',
+    teacherId: 'T004',
+    name: 'Test',
+    joiningDate: '01/01/2021',
+    designation: 'Principal',
+    engagementType: 'Permanent',
+    status: 'Inactive',
+  },
+];
+
+const activatehandler = (record: UserData) => {
+  // eslint-disable-next-line no-console
+  console.log(record);
+};
+
+const UsersList = () => {
+  const onChange = (
+    pagination: unknown,
+    filters: unknown,
+    sorter: unknown,
+    extra: unknown,
+  ) => {
+    // alert('onChange');
+    // eslint-disable-next-line no-console
+    console.log('params', pagination, filters, sorter, extra);
+  };
+  return <Table columns={columns} dataSource={data} onChange={onChange} />;
+};
+
+export default UsersList;
+
+/**
+ * 
+ *  {
+    title: 'Name',
+    dataIndex: 'name',
+    render: (name: string) => ({ children: name }),
+    ...tableColumnTextFilterConfig<UserData>(),
+    onFilter: (value, record) => {
+      return record.name
+        .toString()
+        .toLowerCase()
+        .includes(value.toString().toLowerCase());
+    },
+  },
+ */
+
+/**
+   * 
+   * // interface CustomColumn {
 //   title: string | React.ReactNode;
 //   dataIndex: string | string[];
 //   // sorter?: { compare: (a: UserData, b: UserData) => number };
@@ -50,83 +213,4 @@ interface UserData {
 //     dataIndex: 'status',
 //   },
 // ];
-
-const columns: ColumnType<UserData>[] = [
-  {
-    title: 'Teacher Id',
-    dataIndex: 'teacherId',
-    sorter: true,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    render: (name: string) => ({ children: name }),
-    ...tableColumnTextFilterConfig<UserData>(),
-    onFilter: (value, record) => {
-      return record.name
-        .toString()
-        .toLowerCase()
-        .includes(value.toString().toLowerCase());
-    },
-  },
-
-  {
-    title: 'Designation',
-    dataIndex: 'designation',
-  },
-  {
-    title: 'Joining Date',
-    dataIndex: 'joiningDate',
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-  },
-];
-
-const data: UserData[] = [
-  {
-    key: '1',
-    teacherId: 'T001',
-    name: 'John Brown',
-    joiningDate: '01/01/2020',
-    designation: 'Principal',
-  },
-  {
-    key: '2',
-    teacherId: 'T002',
-    name: 'Brown',
-    joiningDate: '01/01/2020',
-    designation: 'Principal',
-  },
-  {
-    key: '3',
-    teacherId: 'T003',
-    name: 'Jon',
-    joiningDate: '01/01/2020',
-    designation: 'Principal',
-  },
-  {
-    key: '4',
-    teacherId: 'T004',
-    name: 'Test',
-    joiningDate: '01/01/2020',
-    designation: 'Principal',
-  },
-];
-
-const UsersList = () => {
-  const onChange = (
-    pagination: unknown,
-    filters: unknown,
-    sorter: unknown,
-    extra: unknown,
-  ) => {
-    alert('onChange');
-    // eslint-disable-next-line no-console
-    console.log('params', pagination, filters, sorter, extra);
-  };
-  return <Table columns={columns} dataSource={data} onChange={onChange} />;
-};
-
-export default UsersList;
+   */
